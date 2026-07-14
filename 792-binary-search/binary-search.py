c6d653d9eb5840a1cd2111777eba885bab481435
sakshamsuyal <mr.suyal24@gmail.com>
2026-07-14 21:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (35.26%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            middle = (left+right)//2
+            if nums[middle] == target:
+                return middle
+            elif target > nums[middle]:
+                 left = middle+1
+            else:
+                right = middle-1
+        return -1
+        
